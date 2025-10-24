e20bb7991a9fef1b6fca42256b69f26074ccc708
Mathis Chopard <diabloEXE@laposte.net>
2025-10-24 08:27:35 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ const mainContent = document.getElementById("mainContent");
 
 let isAnimating = false;
 
-doorContainer.addEventListener("click", () => {
+function triggerDoorAnimation() { 
   if (isAnimating) {
     return;
   }
@@ -13,14 +13,17 @@ doorContainer.addEventListener("click", () => {
   door.classList.add("open"); // anim d'ouverture
 
   setTimeout(() => {
-    doorContainer.classList.add("zooming"); // zoom après 1.5s
+    doorContainer.classList.add("zooming"); // zoom après 1s
 
     setTimeout(() => {
       doorContainer.style.display = 'none';
       mainContent.classList.add("visible");
       document.body.style.overflow = 'auto';
-      
     }, 1200);
 
   }, 1000);
-});
+}
+
+doorContainer.addEventListener("click", triggerDoorAnimation); //automatique
+
+setTimeout(triggerDoorAnimation, 100); //delay
